3c4af9ef4c438565226ba6467a7f1e1d5f292c97
rathiharshit123 <53534753+rathiharshit123@users.noreply.github.com>
2023-02-01 21:19:29 +0530

    changes

--- backend/server/utils/constants.js
+++ backend/server/utils/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MAX_PRODUCTS_PER_PAGE : 5,
+    MAX_PRODUCTS_PER_PAGE : 8,
     FEILDS_TO_REMOVE_FOR_FILTER: ["page","keyword","limit"],
     RESET_PASSWORD_EXPIRE_TIME: 15*60*1000, //15 minutes,
     ORDER_STATUS: {
