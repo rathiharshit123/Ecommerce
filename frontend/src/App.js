7a4e0bee282993169a43f970a696818a8e87c130
rathiharshit123 <53534753+rathiharshit123@users.noreply.github.com>
2023-02-01 18:12:07 +0530

    Home and product component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Header from "./components/layout/Header/Header.js"
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import WebFont from 'webfontloader';
 import React from 'react';
 import Footer from './components/layout/Footer/Footer';
-
+import Home from './components/Home/Home';
 
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
   return (
     <Router>
       <Header/>
-
+      <Route exact path="/" component={Home}/>
       <Footer/>
     </Router>
   );
