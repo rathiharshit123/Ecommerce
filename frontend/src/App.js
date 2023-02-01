903b1249facad75b3db5d890c4d0a1f1e152c067
rathiharshit123 <53534753+rathiharshit123@users.noreply.github.com>
2023-02-01 15:13:53 +0530

    Header component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import Header from "./components/layout/Header/Header.js"
+import {BrowserRouter as Router} from 'react-router-dom'
+import WebFont from 'webfontloader';
+import React from 'react';
+
+
 
 function App() {
+
+  React.useEffect(()=>{
+    WebFont.load({
+      google:{
+        families: ["Roboto", "Droid Sans", "Chilanka"]
+      }
+    })
+  },[]);
+
   return (
-    <div>
-    
-    </div>
+    <Router>
+      <Header/>
+    </Router>
   );
 }
 
