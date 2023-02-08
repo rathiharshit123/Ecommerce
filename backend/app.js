0895bc528ed0d49845980e900e72552ba2552809
rathiharshit123 <53534753+rathiharshit123@users.noreply.github.com>
2023-02-08 23:23:28 +0530

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const bodyParser = require("body-parser");
 
 const express = require("express");
 const app = express();
+app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(fileUpload());
