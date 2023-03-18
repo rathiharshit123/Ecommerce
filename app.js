9f4176f2ab8aa92b64c3954b0a1cd357220868ac
rathiharshit123 <53534753+rathiharshit123@users.noreply.github.com>
2023-03-19 00:58:36 +0530

    TEstting for deployment

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ app.use(cookieParser());
 app.use(fileUpload());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname,"./frontend/build")))
+app.use(express.static(path.join(__dirname,"./client/build")))
 app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./frontend/build/index.html"))
+    res.sendFile(path.resolve(__dirname,"./client/build/index.html"))
 })
 
 cloudinary.config(config.cloudinary);
