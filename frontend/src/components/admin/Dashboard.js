ef2df1bed2318f9240b1e18fa71775855cf35708
rathiharshit123 <53534753+rathiharshit123@users.noreply.github.com>
2023-03-19 17:45:49 +0530

    Bug fixes and improvements

--- frontend/src/components/admin/Dashboard.js
+++ frontend/src/components/admin/Dashboard.js
@@ -30,6 +30,11 @@ const Dashboard = () => {
     products && products.forEach((product)=>{
         if(!product.stock) outOfStock++;
     })
+    
+    let totalAmount = 0;
+    orders?.forEach(order => {
+      totalAmount+=order.totalPrice
+    });
 
     const lineState = {
         labels: ["Initial Amount", "Amount Earned"],
@@ -38,7 +43,7 @@ const Dashboard = () => {
             label: "TOTAL AMOUNT",
             backgroundColor: ["tomato"],
             hoverBackgroundColor: ["rgb(197, 72, 49)"],
-            data: [0, 4000],
+            data: [0, totalAmount],
           },
         ],
       };
@@ -49,15 +54,11 @@ const Dashboard = () => {
           {
             backgroundColor: ["#00A6B4", "#6800B4"],
             hoverBackgroundColor: ["#4B5000", "#35014F"],
-            data: [outOfStock, products.length - outOfStock],
+            data: [outOfStock, products?.length - outOfStock],
           },
         ],
       };
 
-      let totalAmount = 0;
-      orders?.forEach(order => {
-        totalAmount+=order.totalPrice
-      });
 
   return (
 
